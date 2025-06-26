3419511f38ff0e67326271c88ed6009b694d15d2
Manikandaraja06 <mmani29345@gmail.com>
2025-06-26 16:36:15 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+console.log("answer is not correct")
+
+
+
+const num1 = 10;
+const num2 = 5;
+const sum = num1 + num2;
+console.log("The sum is:", sum); // Output: The sum is: 15
